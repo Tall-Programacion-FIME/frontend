86a35cd0133bb7000b1959f1ca4e337cf6202911
HectorMtz22 <hec.gre81@gmail.com>
2021-03-03 01:39:45 -0600

    Header Component Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import "./scss/style.scss";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <header>
-      <h1>Hello World!</h1>
-    </header>
-  );
+  return <Header></Header>;
 }
 
 export default App;
