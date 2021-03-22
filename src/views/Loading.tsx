31f0837ebdfbeb3ab8ff700d1dae78ae5458c80f
HectorMtz22 <hec.gre81@gmail.com>
2021-03-22 00:16:03 -0600

    Styles on Loading Page

--- src/views/Loading.tsx
+++ src/views/Loading.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Loading() {
   return (
-    <>
+    <div className="loading">
       <h1>Loading page...</h1>
       <p>Please Wait</p>
-    </>
+    </div>
   );
 }
