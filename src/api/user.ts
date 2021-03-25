cc2205dbbd0d3cc57230e0f41f425667a0ebc333
HectorMtz22 <hec.gre81@gmail.com>
2021-03-25 00:39:05 -0600

    User API Functions

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,10 +1,32 @@
 import axios from 'axios';
+import {UserModel, RegisterModel} from "../models/user"
 	
 const path = process.env.REACT_APP_PRO_MODE; 
 
-export async function user() {
-	let user = await axios.get(path + "");
-	console.log(user);
 
-	return user;
+export async function getUsers(): Promise<UserModel[]> {
+  let response = await axios.get<UserModel[]>(path + "user/");
+  return response.data
 }
+
+export async function getUser(_id: number): Promise<UserModel[]> {
+  let response = await axios.get<UserModel[]>(path + "user/" + _id);
+  return response.data
+}
+
+export async function getMyInfo(): Promise<UserModel[]> {
+  let response = await axios.get<UserModel[]>(path + "user/me/");
+  return response.data
+}
+
+export async function postUser(data: RegisterModel): Promise<RegisterModel[]> {
+  let response = await axios.post<RegisterModel[]>(path + "user/", data);
+  return response.data
+}
+
+export async function banUser(_id: number): Promise<UserModel[]> {
+  let response = await axios.delete<UserModel[]>(path + "user/" + _id + "/ban");
+  return response.data
+}
+
+
---
+++ src/models/user.ts
@@ -0,0 +1,19 @@
+export type UserModel = {
+  name: string
+  email: string
+  id: number
+  is_active: boolean
+  is_admin: boolean
+	books_for_sale: object
+}
+
+export type RegisterModel = {
+  name: string
+  email: string
+	password: string
+}
+
+export type LoginModel = {
+  email: string
+	password: string
+}
