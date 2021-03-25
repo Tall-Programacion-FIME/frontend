10bd188dde6fbd7ddfceec3c14583f31fe2604e4
HectorMtz22 <hec.gre81@gmail.com>
2021-03-25 01:27:40 -0600

    Add Link stuff

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import SearchBox from "./SearchBox";
 
 function Header() {
   return (
     <header>
-      <h1>FIME sobre ruedas</h1>
+      <Link to="/">
+        <h1>FIME sobre ruedas</h1>
+      </Link>
       <SearchBox />
     </header>
   );
