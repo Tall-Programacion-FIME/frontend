8e197abd0932d171d1d4d1260b036d7bc0242fd3
HectorMtz22 <hec.gre81@gmail.com>
2021-03-25 01:03:41 -0600

    Provisional Footer, only displays info

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,23 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <p>Hecho por:</p>
-      <p>Héctor Mauricio Flores Martínez</p>
-      <p>Sebastián Marines Álvarez</p>
+      <p>
+        Hecho por: <del>Provisiontal Footer</del>
+      </p>
+      <table>
+        <tr>
+          <th>Nombre</th>
+          <th>Matrícula</th>
+        </tr>
+        <tr>
+          <td>Héctor Mauricio Flores Martínez</td>
+          <td>1897759</td>
+        </tr>
+        <tr>
+          <td>Sebastián Marines Álvarez</td>
+          <td>2077782</td>
+        </tr>
+      </table>
     </footer>
   );
 }
