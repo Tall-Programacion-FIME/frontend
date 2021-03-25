69d7a45549ea08f6d45acd8bc3467fe5397275d1
HectorMtz22 <hec.gre81@gmail.com>
2021-03-25 00:51:28 -0600

    New Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, {lazy, Suspense} from "react"; // React Core and Utils
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"; // Web Router
+import React, { lazy, Suspense } from "react"; // React Core and Utils
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"; // Web Router
 import "./scss/style.scss"; // Import of the Styles of the app
 import Loading from "./views/Loading"; // Fallback, this goes from a traditional import
 
@@ -7,18 +7,20 @@ const Views = "./views/"; // Path of Views
 
 // Code Splitting
 const Header = lazy(() => import("./components/Header")); // Header Section
+const Footer = lazy(() => import("./components/Footer")); // Footer Section
 const Home = lazy(() => import(Views + "Home")); // Homepage view
 const About = lazy(() => import(Views + "About")); // About Section
 
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<Loading/>}>
-        <Header/>
+      <Suspense fallback={<Loading />}>
+        <Header />
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
         </Switch>
+        <Footer />
       </Suspense>
     </Router>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Hecho por:</p>
+      <p>Héctor Mauricio Flores Martínez</p>
+      <p>Sebastián Marines Álvarez</p>
+    </footer>
+  );
+}
+
+export default Footer;
