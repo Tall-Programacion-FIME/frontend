ece68e1cc45ff21d63c755f0142b858b566568c5
HectorMtz22 <hec.gre81@gmail.com>
2021-03-24 23:45:57 -0600

    New functions

--- src/api/book.ts
+++ src/api/book.ts
@@ -8,6 +8,26 @@ export async function getBooks(): Promise<BookModel[]> {
   return response.data
 }
 
+export async function getBook(_id: string): Promise<BookModel[]> {
+  let response = await axios.get<BookModel[]>(path + "book/" + _id);
+  return response.data
+}
+
+export async function postBook(data: BookModel) {
+	let response = await axios.post<BookModel[]>(path + "book/create", data);
+	return response.data;
+}
+
+export async function updateBook(data: BookModel) {
+	let response = await axios.put<BookModel[]>(path + "book/" + data.id, data);
+	return response.data;
+}
+
+export async function deleteBook(_id: string) {
+	let response = await axios.delete<BookModel[]>(path + "book/" + _id);
+	return response.data;
+}
+
 export async function searchBook(query: string) {
   let response = await axios.get<BookModel[]>(
     path + "book/search/",
