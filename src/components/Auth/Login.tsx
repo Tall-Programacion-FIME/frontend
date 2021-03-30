0cdf2128acdea536938cc0007a27b68d6111776a
HectorMtz22 <hec.gre81@gmail.com>
2021-03-30 00:39:54 -0600

    Design of Forms for Auth

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -2,9 +2,24 @@ import React from "react";
 
 function Login() {
   return (
-    <>
+    <form className="form form_login">
       <h2>Inicia Sesión</h2>
-    </>
+      <label htmlFor="email">Email:</label>
+      <input
+        type="text"
+        name="email"
+        id="email"
+        placeholder="Ingresa tu email"
+      />
+      <label htmlFor="password">Contraseña:</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Ingresa tu contraseña"
+      />
+      <button type="submit">Iniciar Sesión</button>
+    </form>
   );
 }
 
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -2,9 +2,40 @@ import React from "react";
 
 function Register() {
   return (
-    <>
+    <form className="form form_register">
       <h2>Registrate</h2>
-    </>
+      <label htmlFor="name">Nombre:</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Ingresa tu Nombre"
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Ingresa tu email"
+      />
+      <label htmlFor="password">Contraseña:</label>
+      <input
+        type="password"
+        name="password"
+        id="password"
+        placeholder="Ingresa tu contraseña"
+      />
+      <label htmlFor="confirm-password">Confirmar Contraseña:</label>
+      <input
+        type="password"
+        name="confirm-password"
+        id="confirm-password"
+        placeholder="Ingresa tu contraseña"
+      />
+      <input type="checkbox" name="terms" id="terms" />
+      <label htmlFor="terms">Acepto los términos y condiciones</label>
+      <button type="submit">Regístrate</button>
+    </form>
   );
 }
 
