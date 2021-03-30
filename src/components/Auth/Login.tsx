febc11b6cc682fb1cb3639a14b41eec71f26f190
HectorMtz22 <hec.gre81@gmail.com>
2021-03-30 11:27:50 -0600

    ReStyling on Forms

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 function Login() {
   return (
-    <form className="form form_login">
+    <form className="form_fullscreen">
       <h2>Inicia Sesión</h2>
       <label htmlFor="email">Email:</label>
       <main className="input_container">
-        <span className="content_span">Ingresa tu email</span>
-        <input type="text" name="email" id="email" />
+        <input type="text" name="email" id="email" required />
+        <label htmlFor="email" className="label_name">
+          <span className="content_span">Ingresa tu email</span>
+        </label>
       </main>
       <label htmlFor="password">Contraseña:</label>
       <main className="input_container">
-        <span className="content_span">Ingresa tu contraseña</span>
-        <input type="password" name="password" id="password" />
+        <input type="password" name="password" id="password" required />
+        <label htmlFor="password" className="label_name">
+          <span className="content_span">Ingresa tu contraseña</span>
+        </label>
       </main>
       <button type="submit">Iniciar Sesión</button>
     </form>
--- src/components/Auth/Register.tsx
+++ src/components/Auth/Register.tsx
@@ -2,36 +2,41 @@ import React from "react";
 
 function Register() {
   return (
-    <form className="form form_register">
+    <form className="form_fullscreen">
       <h2>Registrate</h2>
       <label htmlFor="name">Nombre:</label>
-      <input
-        type="text"
-        name="name"
-        id="name"
-        placeholder="Ingresa tu Nombre"
-      />
+      <main className="input_container">
+        <input type="text" name="name" id="name" required />
+        <label htmlFor="name" className="label_name">
+          <span className="content_span">Ingresa tu nombre</span>
+        </label>
+      </main>
       <label htmlFor="email">Email:</label>
-      <input
-        type="email"
-        name="email"
-        id="email"
-        placeholder="Ingresa tu email"
-      />
+      <main className="input_container">
+        <input type="text" name="email" id="email" required />
+        <label htmlFor="email" className="label_name">
+          <span className="content_span">Ingresa tu email</span>
+        </label>
+      </main>
       <label htmlFor="password">Contraseña:</label>
-      <input
-        type="password"
-        name="password"
-        id="password"
-        placeholder="Ingresa tu contraseña"
-      />
+      <main className="input_container">
+        <input type="password" name="password" id="password" required />
+        <label htmlFor="password" className="label_name">
+          <span className="content_span">Ingresa tu Contraseña</span>
+        </label>
+      </main>
       <label htmlFor="confirm-password">Confirmar Contraseña:</label>
-      <input
-        type="password"
-        name="confirm-password"
-        id="confirm-password"
-        placeholder="Ingresa tu contraseña"
-      />
+      <main className="input_container">
+        <input
+          type="password"
+          name="confirm-password"
+          id="confirm-password"
+          required
+        />
+        <label htmlFor="confirm-password" className="label_name">
+          <span className="content_span">Confirma tu Contraseña</span>
+        </label>
+      </main>
       <input type="checkbox" name="terms" id="terms" />
       <label htmlFor="terms">Acepto los términos y condiciones</label>
       <button type="submit">Regístrate</button>
