5e8becdeced791efed6af3a84fbca3e149af8c0e
HectorMtz22 <hec.gre81@gmail.com>
2021-04-02 23:41:04 -0600

    Dev Print Token

--- src/components/Auth/AuthSatisfied.tsx
+++ src/components/Auth/AuthSatisfied.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import useStore from "../../store/Auth";
 
+const saveOnLocal = (access_token: string, refresh_token: string) => {
+  window.localStorage.setItem(access_token, access_token);
+  window.localStorage.setItem(refresh_token, refresh_token);
+};
+
 function AuthSatisfied() {
   const { access_token, refresh_token, isAuthenticated } = useStore();
+  if (isAuthenticated) {
+    saveOnLocal(access_token, refresh_token);
+  }
 
   return (
     <div>
       <h1>Get</h1>
       <h1>{access_token}</h1>
       <h2>{refresh_token}</h2>
-      <p>""{isAuthenticated}</p>
+      <p>{isAuthenticated}</p>
     </div>
   );
 }
