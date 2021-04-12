deae5b1317d8d50e39aaa34b6941063ada13415b
HectorMtz22 <hec.gre81@gmail.com>
2021-04-11 22:52:41 -0500

    Store Warnings

--- src/store/MessageState.ts
+++ src/store/MessageState.ts
@@ -3,13 +3,17 @@ import create from 'zustand'
 type State = {
 	isMessage: boolean 
 	message: string 
+	typeMessage?: string
+	isPermanent?: boolean 
 	//setData: any
 }
 
 
 const useStore = create<State>((set, get) => ({
 	message: '',
-	isMessage: false 
+	isMessage: false, 
+	typeMessage: '',
+	isPermanent: true
 }))
 
 export default useStore
