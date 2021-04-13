b42079d41ce56ee5ed11b55069ef1770d7d0d655
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-13 09:02:19 -0500

    Correct return types in user API functions

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {UserModel, RegisterModel} from "../models/user"
-	
-const path = process.env.REACT_APP_PRO_MODE; 
+
+const path = process.env.REACT_APP_PRO_MODE;
 
 
 export async function getUsers(): Promise<UserModel[]> {
@@ -9,23 +9,23 @@ export async function getUsers(): Promise<UserModel[]> {
   return response.data
 }
 
-export async function getUser(_id: number): Promise<UserModel[]> {
-  let response = await axios.get<UserModel[]>(path + "user/" + _id);
+export async function getUser(_id: number): Promise<UserModel> {
+  let response = await axios.get<UserModel>(path + "user/" + _id);
   return response.data
 }
 
-export async function getMyInfo(token: string): Promise<UserModel[]> {
-  let response = await axios.get<UserModel[]>(path + "user/me", {headers: {'Authorization': `Bearer ${token}`}});
+export async function getMyInfo(token: string): Promise<UserModel> {
+  let response = await axios.get<UserModel>(path + "user/me", {headers: {'Authorization': `Bearer ${token}`}});
   return response.data
 }
 
-export async function postUser(data: RegisterModel): Promise<RegisterModel[]> {
-  let response = await axios.post<RegisterModel[]>(path + "user/", data);
+export async function postUser(data: RegisterModel): Promise<RegisterModel> {
+  let response = await axios.post<RegisterModel>(path + "user/", data);
   return response.data
 }
 
-export async function banUser(_id: number): Promise<UserModel[]> {
-  let response = await axios.delete<UserModel[]>(path + "user/" + _id + "/ban");
+export async function banUser(_id: number): Promise<UserModel> {
+  let response = await axios.delete<UserModel>(path + "user/" + _id + "/ban");
   return response.data
 }
 
