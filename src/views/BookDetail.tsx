fcf8fdf71396a131664710fc4e322ce80d97ee71
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-14 13:14:10 -0500

    Redesign book detail page

--- src/views/BookDetail.tsx
+++ src/views/BookDetail.tsx
@@ -30,13 +30,24 @@ function BookDetail() {
 
   return (
     <div className="detail-layout">
-      <img src={book?.cover_url} alt={book?.name}/>
-      <aside>
-        <h2>{book?.name}</h2>
-        <p>Precio: <b>{book?.price}</b></p>
-        <p>Autor: {book?.author}</p>
-        <p>Vendido por: {user?.name} ({user?.email})</p>
-      </aside>
+      <div className="card">
+        <div className="img-container">
+          <img src={book?.cover_url} alt={`${book?.name} - ${book?.author}`}/>
+        </div>
+        <div className="product-content">
+          <h2 className="product-title">{book?.name}</h2>
+          <h2>Autór(es): <span>{book?.author}</span></h2>
+          <span>Precio: <b>$ {book?.price}</b></span>
+
+          <div style={{marginBottom: "2rem"}}/>
+
+          <h1>Datos del vendedor</h1>
+          <hr />
+          <h2>{user?.name}</h2>
+          <h4>Contacto: {user?.email}</h4>
+          <div style={{marginBottom: "2rem"}}/>
+        </div>
+      </div>
     </div>
   );
 }
