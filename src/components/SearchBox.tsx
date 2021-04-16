12eb47d3e54f666aa124abfd85bfd3ed06a437cc
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-15 20:44:27 -0500

    Create styling for dropdown

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -47,6 +47,15 @@ export default function SearchBox() {
           <FontAwesomeIcon icon="search" />
         </button>
       </form>
+      <div className="dropdown">
+          <div className="dropdown-content">
+            <a href="/">Link 1</a>
+            <a href="/">Link 2</a>
+            <a href="/">Link 3</a>
+            <a href="/">Link 3</a>
+            <a href="/">Link 3</a>
+          </div>
+      </div>
     </div>
   );
 }
