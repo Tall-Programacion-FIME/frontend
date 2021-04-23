470e706e0e389b46d71bdb6f35d2efbf649ba2e1
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-23 13:51:28 -0500

    Do not make an API request if the user details are already cached

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -5,28 +5,36 @@ import userStore from "../store/User";
 
 function Profile() {
   const { access_token } = useStore();
-  const { name, id, email } = userStore();
+  const { name, email } = userStore();
 
   // useEffect Hook, used for async functions
   useEffect(() => {
     let mounted = true;
-    (async () => {
-      const user_info = await getMyInfo(access_token);
-      if (mounted && user_info) {
-        userStore.setState(user_info);
-      }
-    })();
+    // Check if user details are already cached
+    if (!name) {
+      console.log("Requesting");
+      (async () => {
+        const user_info = await getMyInfo(access_token);
+        if (mounted && user_info) {
+          userStore.setState(user_info);
+        }
+      })();
+    }
     return () => {
       mounted = false;
     };
   }, [access_token]);
 
   return (
-    <div className="form_fullscreen">
-      <h1>Nombre: {name}</h1>
-      <h2>Id: {id}</h2>
-      <h2>Email: {email}</h2>
-    </div>
+    <>
+      <div className="form_fullscreen">
+        <h1>Nombre: {name}</h1>
+        <h2>Email: {email}</h2>
+      </div>
+      <div>
+
+      </div>
+    </>
   );
 }
 
