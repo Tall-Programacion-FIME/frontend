f3047c2bafa81779dfce1190d492b44aa49619a3
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-23 16:08:29 -0500

    Show input fields on edit

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import { getMyInfo } from "../api/user";
 import useStore from "../store/Auth";
 import userStore from "../store/User";
@@ -7,6 +7,13 @@ import {deleteBook} from "../api/book";
 function Profile() {
   const { access_token } = useStore();
   const { name, email, books_for_sale, updateBooks } = userStore();
+  const [editing, setEditing] = useState<number | null>(null);
+
+  const [currentBook, setCurrentBook] = useState({
+    name: '',
+    author: '',
+    price: 0,
+  })
 
   // useEffect Hook, used for async functions
   useEffect(() => {
@@ -31,6 +38,23 @@ function Profile() {
     updateBooks(new_books)
   }
 
+  const handleEditBook = async (book_id: number) => {
+    setEditing(book_id)
+    let book = books_for_sale.reduce((acc, curr) => {
+      if (curr.id == book_id) {
+        return curr
+      }
+      return acc
+    })
+    if (book !== null) {
+      setCurrentBook(book)
+    }
+  }
+
+  const handleSaveBook = () => {
+    setEditing(null)
+  }
+
   return (
     <div className="my-books">
       <div className="form_fullscreen">
@@ -51,16 +75,64 @@ function Profile() {
           {books_for_sale.map(book => (
             <tr key={book.id}>
               <td data-label="Titulo">
-                <span style={{paddingRight: "2px"}}>{book.name}</span>
-                <a href={`/book/${book.id}`}>
-                  <img style={{height: "1rem", width: "auto"}} alt={book.name} src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiB3aWR0aD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEwIDEzYTUgNSAwIDAgMCA3LjU0LjU0bDMtM2E1IDUgMCAwIDAtNy4wNy03LjA3bC0xLjcyIDEuNzEiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHN0cm9rZS13aWR0aD0iMiIvPjxwYXRoIGQ9Ik0xNCAxMWE1IDUgMCAwIDAtNy41NC0uNTRsLTMgM2E1IDUgMCAwIDAgNy4wNyA3LjA3bDEuNzEtMS43MSIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjMDAwIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIgc3Ryb2tlLXdpZHRoPSIyIi8+PC9zdmc+" />
-                </a>
+                {
+                  editing === book.id ? (
+                    <>
+                      <input
+                        type="text"
+                        defaultValue={currentBook.name}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <span style={{paddingRight: "2px"}}>{book.name}</span>
+                      <a href={`/book/${book.id}`}>
+                      <img style={{height: "1rem", width: "auto"}} alt={book.name} src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiB3aWR0aD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTEwIDEzYTUgNSAwIDAgMCA3LjU0LjU0bDMtM2E1IDUgMCAwIDAtNy4wNy03LjA3bC0xLjcyIDEuNzEiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbWl0ZXJsaW1pdD0iMTAiIHN0cm9rZS13aWR0aD0iMiIvPjxwYXRoIGQ9Ik0xNCAxMWE1IDUgMCAwIDAtNy41NC0uNTRsLTMgM2E1IDUgMCAwIDAgNy4wNyA3LjA3bDEuNzEtMS43MSIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjMDAwIiBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1taXRlcmxpbWl0PSIxMCIgc3Ryb2tlLXdpZHRoPSIyIi8+PC9zdmc+" />
+                      </a>
+                    </>
+                  )
+                }
               </td>
-              <td data-label="Autor">{book.author}</td>
-              <td data-label="Precio">{book.price}</td>
+              <td data-label="Autor">
+                {
+                  editing === book.id ? (
+                    <>
+                      <input
+                        type="text"
+                        defaultValue={currentBook.author}
+                      />
+                    </>
+                  ) : (
+                    <span>{book.author}</span>
+                  )
+                }
+              </td>
+              <td data-label="Precio">
+                {
+                  editing === book.id ? (
+                    <>
+                      <input
+                        type="number"
+                        defaultValue={currentBook.price}
+                      />
+                    </>
+                  ) : (
+                    <span>{book.price}</span>
+                  )
+                }
+              </td>
               <td>
-                <button className="delete" onClick={() => handleDeleteBook(book.id)}>Eliminar</button>
-                <button>Editar</button>
+                {
+                  editing === book.id ? (
+                    <button onClick={() => handleSaveBook()}>Guardar</button>
+                  ) : (
+                    <>
+                      <button className="delete" onClick={() => handleDeleteBook(book.id)}>Eliminar</button>
+                      <button onClick={() => handleEditBook(book.id)}>Editar</button>
+                    </>
+                  )
+
+                }
               </td>
             </tr>
           ))}
